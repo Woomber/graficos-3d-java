c64e8ec7c7b28cc165727da8699a951bd90e6e2b
Yael Chavoya <yaelchavoya@gmail.com>
2021-01-22 16:53:22 -0600

    Reacomodo

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -18,9 +18,9 @@ import java.awt.image.BufferedImage;
 
 public class MainWindow extends JFrame {
 
+    protected static final double PROJECTION_ANGLE = Math.atan(-2);
+
     protected Dibujante3D dibujante3D;
-    protected Proyectador proyectador;
-    protected Figura figura;
     protected boolean drawn = false;
 
     public MainWindow() {
@@ -28,35 +28,6 @@ public class MainWindow extends JFrame {
         this.setTitle("Proyecciones");
         this.setResizable(false);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        // ===========================
-
-        // Proyección Paralela Ortogonal
-        // figura = new FiguraTetris();
-        //proyectador = new ParallelOrthogonalProjection();
-
-        // ===========================
-
-        // Proyecciones Paralelas Oblicuas
-        double projectionAngle = Math.atan(-2);
-
-        // Proyección Cabinet
-        //figura = new FiguraCubo();
-        //proyectador = new ParallelProjection(0.5 * Math.cos(projectionAngle), 0.5 * Math.sin(projectionAngle));
-
-        // Proyección Cavalier
-        //figura = new FiguraCubo();
-        //proyectador = new ParallelProjection(Math.cos(projectionAngle), Math.sin(projectionAngle));
-
-        // Proyección Isométrica
-        //figura = new FiguraCubo();
-        //proyectador = new ParallelProjection(Math.PI/4, Math.PI/4);
-
-        // ===========================
-
-        figura = new FiguraCubo();
-        proyectador = new PerspectiveProjection(2);
-
         this.setVisible(true);
     }
 
@@ -67,18 +38,35 @@ public class MainWindow extends JFrame {
             dibujante3D = new Dibujante3D(new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_ARGB), this);
             dibujante3D.setOrigin(new Punto2D(getWidth()/2, getHeight()/2));
 
-            dibujante3D.drawAristas(proyectador.proyectar(
-                    figura
+            // Proyección Paralela Ortogonal
+            //proyectar(new FiguraTetris(), new ParallelOrthogonalProjection());
+
+            // Proyecciones Paralelas Oblicuas
+            // Proyección Cabinet
+            // proyectar(new FiguraCubo(), new ParallelProjection(0.5 * Math.cos(PROJECTION_ANGLE), 0.5 * Math.sin(PROJECTION_ANGLE)));
+            // Proyección Cavalier
+            // proyectar(new FiguraCubo(), new ParallelProjection(Math.cos(PROJECTION_ANGLE), Math.sin(PROJECTION_ANGLE)));
+            // Proyección Isométrica
+            // proyectar(new FiguraCubo(), new ParallelProjection(Math.PI/4, Math.PI/4));
+
+            // Proyección Perspectiva
+            proyectar(new FiguraCubo(), new PerspectiveProjection(2));
+
+        }
+    }
+
+    protected void proyectar(Figura figura, Proyectador proyectador) {
+
+        dibujante3D.drawAristas(proyectador.proyectar(
+                figura
                         //.transform(new MatrizTraslacion(20, 20, -75))
                         .transform(new MatrizTraslacion(-25, -25, -75))
                         .transform(new MatrizRotacion(Eje.Y, 30, true))
                         .transform(new MatrizRotacion(Eje.X, -30, true))
                         .transform(new MatrizTraslacion(0, 0, 75))
-            ));
+        ));
 
-            getGraphics().drawImage(dibujante3D.getImage(), 0, 0, this);
-
-        }
+        getGraphics().drawImage(dibujante3D.getImage(), 0, 0, this);
     }
 
     public static void main(String[] args) {
