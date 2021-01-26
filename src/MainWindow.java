5b336419d033b7c929149972802ba7646829c1ff
Yael Chavoya <yaelchavoya@gmail.com>
2021-01-26 00:46:16 -0600

    Números

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,5 +1,6 @@
 import animation.*;
 import dibujante.Dibujante3D;
+import factories.NumberCurveFactory;
 import figuras.*;
 import functions.*;
 import matrices.*;
@@ -42,242 +43,28 @@ public class MainWindow extends JFrame {
             background2D.setColor(new Color(200, 200, 200));
             background2D.drawLine(new Punto2D(0, -getHeight()/2), new Punto2D(0, getHeight()/2));
             background2D.drawLine(new Punto2D(-getWidth()/2, 0), new Punto2D(getWidth()/2, 0));
+            getGraphics().drawImage(background2D.getImage(), 0, 0, this);
 
-            background3D = new Dibujante3D(new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_RGB), this);
-            background3D.setColor(Color.WHITE);
-            background3D.clear();
-            background3D.setOrigin(new Punto2D(getWidth()/2, getHeight()/2));
-            background3D.setColor(new Color(200, 200, 200));
 
-            // Dibujar una de las proyecciones (0 al 3)
-           // dibujarProyecciones(3);
-
-            // Dibujar una de las curvas (0 al 7)
-            // dibujarCurvas(6);
-
-            // Dibujar la malla
-            // dibujarMalla();
-            animationTest();
+            dibujarCurva3D();
         }
     }
 
-    protected void animationTest() {
-        Animation animation = new Animation(getWidth(), getHeight(), this);
-        animation.setFrameAsBackground(background2D.getImage());
-        animation.setOrigin(new Punto2D(getWidth()/2.0, getHeight()/2.0));
-        animation.setFrameDelay(20);
-
-        AnimationElement element = new AnimationElement(new FiguraCubo().transform(new MatrizEscalado(1.5)), new PerspectiveProjection(100));
-
-        int vueltas = 5;
-        for(int i = 0; i < 72 * vueltas; i++) {
-            element.addAction(new MatrizRotacion(Eje.Y, 5, true));
-        }
-        for(int i = 0; i < 72 * vueltas; i++) {
-            element.addAction(new MatrizRotacion(Eje.X, 5, true));
-        }
-
-        animation.addElement(element);
-        AnimationQueue.add(animation);
-
-        AnimationQueue.playOnRepeat(getGraphics());
-    }
-
-    protected void dibujarMalla() {
-        Mesh2D mesh = new MeshRectangular(25, 25, 8);
-        dibujante3D.drawAristas(mesh.getAristas());
-
-        getGraphics().drawImage(dibujante3D.getImage(), 0, 0, this);
-    }
-
-    protected void dibujarCurvas(int numCurva) {
-
-        String out = "Dibujando\n\t";
-
-        switch (numCurva) {
-            // Seno de X (5)
-            case 0:
-                System.out.println(out + "y = sin(x)");
-                dibujarCurva(Functions.create2DCurveExplicit(
-                        (x) -> 200 * Math.sin(x/60),
-                        0,
-                        60.1 * Math.PI,
-                        100,
-                        Functions.MainVariable.X
-                ));
-                break;
-
-            // Coseno de Y (6)
-            case 1:
-                System.out.println(out + "x = y cos(4y)");
-                dibujarCurva(Functions.create2DCurveExplicit(
-                        (y) ->  y * Math.cos(4*y / 15),
-                        0,
-                        30.1 * Math.PI,
-                        100,
-                        Functions.MainVariable.Y
-                ));
-                break;
-
-            // Paramétricas (7)
-            case 2:
-                System.out.println(out + "x = t - 3sin(t)\n\ty = 4 - 3cos(t)");
-                dibujarCurva(Functions.create2DCurveParametric(
-                        (t) -> 10*(t - 3*Math.sin(t)),
-                        (t) -> 10*(4 - 3 * Math.cos(t)),
-                        0,
-                        10,
-                        100
-                ));
-                break;
-
-            // Paramétricas infinito (8)
-            case 3:
-                System.out.println(out + "x = sin(t)/(1 + cos^2(t))\n\ty = sin(t)cos(t)/(1 + cos^2(t))");
-                dibujarCurva(Functions.create2DCurveParametric(
-                        (t) -> 100 * Math.sin(t)/(1 + Math.cos(t) * Math.cos(t)),
-                        (t) -> 100 * Math.sin(t) * Math.cos(t)/(1 + Math.cos(t) * Math.cos(t)),
-                        0,
-                        2.1 * Math.PI,
-                        100
-                ));
-                break;
-
-            // Paramétrica patrón (9)
-            case 4:
-                System.out.println(out + "x = cos(t) + cos(7t)/2 + sin(17t)/3\n\ty = = sin(t) + sin(7t)/2 + cos(17t)/3");
-                dibujarCurva(Functions.create2DCurveParametric(
-                        (t) -> 100 * (Math.cos(t) + 0.5 * Math.cos(7 * t) + Math.sin(17 * t)/3.0),
-                        (t) -> 100 * (Math.sin(t) + 0.5 * Math.sin(7 * t) + Math.cos(17 * t)/3.0),
-                        0,
-                        2.01 * Math.PI,
-                        500
-                ));
-                break;
-
-            // Paramétrica estrella (9)
-            case 5:
-                System.out.println(out + "x = 17cos(t) + 7cos(17t/7)\n\ty = 17sin(t) + 7sin(17t/7)");
-                dibujarCurva(Functions.create2DCurveParametric(
-                        (t) -> 10 * (17 * Math.cos(t) + 7 * Math.cos(17 * t / 7.0)),
-                        (t) -> 10 * (17 * Math.sin(t) - 7 * Math.sin(17 * t / 7.0)),
-                        0,
-                        14.01 * Math.PI,
-                        500
-                ));
-                break;
-
-            // Paramétrica 3D resorte
-            case 6:
-                System.out.println(out + "x = cos(t)\n\ty = sin(t)\n\tz = t");
-                dibujarCurva3D(Functions.create3DCurveParametric(
-                        (t) -> 100 * Math.cos(t),
-                        (t) -> 100 * Math.sin(t),
-                        (t) -> 8 * t,
-                        0,
-                        8.01 * Math.PI,
-                        500
-                ));
-                break;
-
-            // Paramétrica 3D curva
-            case 7:
-                System.out.println(out + "x = cos(3t)\n\ty = 2cos^2(t)\n\tz = sin(2t)");
-                dibujarCurva3D(Functions.create3DCurveParametric(
-                        (t) -> 100 * Math.cos(3 * t),
-                        (t) -> 100 * 2 * Math.cos(t) * Math.cos(t),
-                        (t) -> 100 * Math.sin(2 * t),
-                        - 1.01 * Math.PI,
-                        1.01 * Math.PI,
-                        500
-                ));
-                break;
-
-            default:
-                System.out.println("Número de curva inválido");
-        }
-
-    }
-
-    protected void dibujarCurva(List<Punto2D> puntos) {
-        // Dibujar ejes
-        getGraphics().drawImage(background2D.getImage(), 0, 0, this);
-
-        // Dibujar curva
-        dibujante3D.drawCurve(puntos);
-        getGraphics().drawImage(dibujante3D.getImage(), 0, 0, this);
-    }
-
-    protected void dibujarCurva3D(List<Punto3D> puntos) {
+    protected void dibujarCurva3D() {
         Proyectador proy = new ParallelOrthogonalProjection();
 
-        MatrizTransformacion tz = new MatrizRotacion(Eje.Z, -45, true);
-        MatrizTransformacion tx = new MatrizRotacion(Eje.X, -60, true);
 
-        // Dibujar ejes
-
-        Curva3D ejeX = new Curva3D(), ejeY = new Curva3D(), ejeZ = new Curva3D();
-
-        ejeX.addPuntos(new Punto3D[]{ new Punto3D(0, 0, 0), new Punto3D(500, 0, 0) });
-        ejeY.addPuntos(new Punto3D[]{ new Punto3D(0, 0, 0), new Punto3D(0, 500, 0) });
-        ejeZ.addPuntos(new Punto3D[]{ new Punto3D(0, 0, 0), new Punto3D(0, 0, 500) });
-
-        background3D.drawCurve(proy.proyectar(ejeX.transform(tz).transform(tx)));
-        background3D.drawCurve(proy.proyectar(ejeY.transform(tz).transform(tx)));
-        background3D.drawCurve(proy.proyectar(ejeZ.transform(tz).transform(tx)));
-        getGraphics().drawImage(background3D.getImage(), 0, 0, this);
-
-        // Dibujar curva
-
-        dibujante3D.drawCurve(proy.proyectar(new Curva3D(puntos).transform(tz).transform(tx)));
-        getGraphics().drawImage(dibujante3D.getImage(), 0, 0, this);
-    }
-
-    protected void dibujarProyecciones(int numProy) {
-        String out = "Usando proyección ";
-
-        switch (numProy) {
-
-            // Proyección Paralela Ortogonal
-            case 0:
-                System.out.println(out + "paralela ortogonal");
-                proyectar(new FiguraTetris(), new ParallelOrthogonalProjection());
-                break;
-
-            // Proyecciones Paralelas Oblicuas (1 al 3)
-            // Proyección Cabinet
-            case 1:
-                System.out.println(out + "paralela oblicua Cabinet");
-                proyectar(new FiguraCubo(), new ParallelProjection(0.5 * Math.cos(PROJECTION_ANGLE), 0.5 * Math.sin(PROJECTION_ANGLE)));
-                break;
-
-            // Proyección Cavalier
-            case 2:
-                System.out.println(out + "paralela oblicua Cavalier");
-                proyectar(new FiguraCubo(), new ParallelProjection(Math.cos(PROJECTION_ANGLE), Math.sin(PROJECTION_ANGLE)));
-                break;
-
-            // Proyección Perspectiva
-            case 3:
-                System.out.println(out + "perspectiva");
-                proyectar(new FiguraCubo(), new PerspectiveProjection(100));
-                break;
-
-            default:
-                System.out.println("Número de proyección inválido");
+        final double scale = 7.5;
+        for(int i = 0; i < 10; i++) {
+            // Dibujar curva
+            List<Curva3D> curvas = NumberCurveFactory.buildDigit(i);
+            for (Curva3D c: curvas) {
+                dibujante3D.drawCurve(proy.proyectar(c
+                        .transform(new MatrizEscalado(scale))
+                        .transform(new MatrizTraslacion(380 - 2.5 * scale * i, -220 - scale*3, 0))
+                ));
+            }
         }
-
-    }
-
-    protected void proyectar(Figura figura, Proyectador proyectador) {
-
-        dibujante3D.drawAristas(proyectador.proyectar(
-                figura
-                        .transform(new MatrizEscalado(1.1))
-                        .transform(new MatrizRotacion(Eje.Y, 30, true))
-                        .transform(new MatrizRotacion(Eje.X, -30, true))
-                        .transform(new MatrizTraslacion(40, 40, 0))
-        ));
 
         getGraphics().drawImage(dibujante3D.getImage(), 0, 0, this);
     }
---
+++ src/factories/NumberCurveFactory.java
@@ -0,0 +1,139 @@
+package factories;
+
+import figuras.Curva3D;
+import functions.Functions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NumberCurveFactory extends Curva3D {
+
+    public static List<Curva3D> buildDigit(int digit) throws ArrayIndexOutOfBoundsException {
+        List<Curva3D> curvas = new ArrayList<>();
+        switch (digit) {
+            case 0:
+                curvas.add(new Curva3D(Functions.create3DCurveParametric(
+                        t -> Math.cos(t), t -> 2 * Math.sin(t), t -> 0,
+                        0, 2.1 * Math.PI, 100
+                )));
+                break;
+            case 1:
+                curvas.add(new Curva3D(Functions.create3DCurveParametric(
+                        t -> 0, t -> t, t -> 0,
+                        -2, 2, 2
+                )));
+                curvas.add(new Curva3D(Functions.create3DCurveParametric(
+                        t -> t, t -> -2, t -> 0,
+                        -1, 1, 2
+                )));
+                curvas.add(new Curva3D(Functions.create3DCurveParametric(
+                        t -> t - 2, t -> t, t -> 0,
+                        1, 2, 2
+                )));
+                break;
+            case 2:
+                curvas.add(new Curva3D(Functions.create3DCurveParametric(
+                        t -> Math.cos(t), t -> Math.sin(t) + 1, t -> 0,
+                        0, Math.PI, 100
+                )));
+                curvas.add(new Curva3D(Functions.create3DCurveParametric(
+                        t -> t, t -> (3*t - 1)/2, t -> 0,
+                        -1, 1, 2
+                )));
+                curvas.add(new Curva3D(Functions.create3DCurveParametric(
+                        t -> t, t -> -2, t -> 0,
+                        -1, 1, 2
+                )));
+                break;
+            case 3:
+                curvas.add(new Curva3D(Functions.create3DCurveParametric(
+                        t -> Math.cos(t), t -> Math.sin(t) + 1, t -> 0,
+                        - Math.PI/2.0, 3.0 / 4.0 * Math.PI, 100
+                )));
+                curvas.add(new Curva3D(Functions.create3DCurveParametric(
+                        t -> Math.cos(t), t -> Math.sin(t) - 1, t -> 0,
+                        - 3.0 / 4.0 * Math.PI, Math.PI/2.0, 100
+                )));
+                break;
+            case 4:
+                curvas.add(new Curva3D(Functions.create3DCurveParametric(
+                        t -> 1, t -> 2*t, t -> 0,
+                        -1, 1, 2
+                )));
+                curvas.add(new Curva3D(Functions.create3DCurveParametric(
+                        t -> t, t -> 0, t -> 0,
+                        -1, 1, 2
+                )));
+                curvas.add(new Curva3D(Functions.create3DCurveParametric(
+                        t -> t, t -> 2 * t + 2, t -> 0,
+                        -1, 0, 2
+                )));
+                break;
+            case 5:
+                curvas.add(new Curva3D(Functions.create3DCurveParametric(
+                        t -> Math.cos(t), t -> (5 * Math.sin(t) - 3)/4.0, t -> 0,
+                        -7.0/8.0 * Math.PI, 1.1 * Math.PI/2.0, 100
+                )));
+                curvas.add(new Curva3D(Functions.create3DCurveParametric(
+                        t -> t, t -> 0.5, t -> 0,
+                        -1, 0, 2
+                )));
+                curvas.add(new Curva3D(Functions.create3DCurveParametric(
+                        t -> t, t -> 2, t -> 0,
+                        -1, 1, 2
+                )));
+                curvas.add(new Curva3D(Functions.create3DCurveParametric(
+                        t -> -1, t -> t, t -> 0,
+                        0.5, 2, 2
+                )));
+                break;
+            case 6:
+                curvas.add(new Curva3D(Functions.create3DCurveParametric(
+                        t -> Math.cos(t), t -> Math.sin(t) - 1, t -> 0,
+                        0, 2.1 * Math.PI, 100
+                )));
+                curvas.add(new Curva3D(Functions.create3DCurveParametric(
+                        t -> 2 * Math.cos(t) + 1, t -> 3 * Math.sin(t) - 1, t -> 0,
+                        0.5 * Math.PI, 1.05 * Math.PI, 100
+                )));
+                break;
+            case 7:
+                curvas.add(new Curva3D(Functions.create3DCurveParametric(
+                        t -> t, t -> 0, t -> 0,
+                        -0.5, 0.5, 2
+                )));
+                curvas.add(new Curva3D(Functions.create3DCurveParametric(
+                        t -> t, t -> 2, t -> 0,
+                        -1, 1, 2
+                )));
+                curvas.add(new Curva3D(Functions.create3DCurveParametric(
+                        t -> t, t -> 2 * t, t -> 0,
+                        -1, 1, 2
+                )));
+                break;
+            case 8:
+                curvas.add(new Curva3D(Functions.create3DCurveParametric(
+                        t -> Math.cos(t), t -> Math.sin(t) + 1, t -> 0,
+                        0,  2.05 * Math.PI, 100
+                )));
+                curvas.add(new Curva3D(Functions.create3DCurveParametric(
+                        t -> Math.cos(t), t -> Math.sin(t) - 1, t -> 0,
+                        0 * Math.PI, 2.05 * Math.PI, 100
+                )));
+                break;
+            case 9:
+                curvas.add(new Curva3D(Functions.create3DCurveParametric(
+                        t -> Math.cos(t), t -> Math.sin(t) + 1, t -> 0,
+                        0, 2.1 * Math.PI, 100
+                )));
+                curvas.add(new Curva3D(Functions.create3DCurveParametric(
+                        t -> 2 * Math.cos(t) - 1, t -> 3 * Math.sin(t) + 1, t -> 0,
+                        -0.5 * Math.PI, 0.05 * Math.PI, 100
+                )));
+                break;
+            default:
+                throw new ArrayIndexOutOfBoundsException("Dígito debe ser entre 0 y 9");
+        }
+        return curvas;
+    }
+}
